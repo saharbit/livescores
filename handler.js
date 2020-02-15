e9c0021f93cce67599d4d52e57a29eaaeee6f3b3
Sahar <sahar.bit@gmail.com>
2020-02-15 18:29:18 +0200

    use dotenv plugin

--- handler.js
+++ handler.js
@@ -1,21 +1,4 @@
 import { server } from "./server";
-import { config } from "dotenv";
-
-config();
-
-export async function hello(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
-}
 
 export const graphqlHandler = server.createHandler({
   cors: {
