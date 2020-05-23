1b6662bda7fd95001f4b5687fec90738d67de0de
Sahar <sahar.bit@gmail.com>
2020-05-23 19:52:55 +0300

    Layout changes

--- web/src/wizard/components/WizardContainer.tsx
+++ web/src/wizard/components/WizardContainer.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-    padding-bottom: 44px; // Continue button height
-`;
+const Container = styled.div``;
 
 const WizardContainer: React.FC = ({ children }) => {
     return <Container className="container mx-auto xl:max-w-screen-lg">{children}</Container>;
--- web/src/wizard/components/WizardList.tsx
+++ web/src/wizard/components/WizardList.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const List = styled.div`
+    max-height: calc(100vh - 96px);
+`;
 
 const WizardList: React.FC = ({ children }) => {
-    return <div className="grid gap-2 grid-cols-3 md:grid-cols-6 lg:grid-cols-8">{children}</div>;
+    return (
+        <List className="grid gap-2 grid-cols-3 md:grid-cols-6 lg:grid-cols-8 overflow-auto p-2 lg:pl-0">
+            {children}
+        </List>
+    );
 };
 
 export default WizardList;
