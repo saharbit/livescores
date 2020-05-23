3d1bdbb455808dea5cf36cf08add36a5f349a997
Sahar <sahar.bit@gmail.com>
2020-05-23 13:45:40 +0300

    Pad container

--- web/src/wizard/components/WizardContainer.tsx
+++ web/src/wizard/components/WizardContainer.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    padding-bottom: 44px; // Continue button height
+`;
 
 const WizardContainer: React.FC = ({ children }) => {
     return <Container className="container mx-auto xl:max-w-screen-lg">{children}</Container>;
