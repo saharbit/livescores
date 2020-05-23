32476252d4f0db4844ad94822a2c3df47f886f78
Sahar <sahar.bit@gmail.com>
2020-05-23 13:49:53 +0300

    Pointer-events none

--- web/src/wizard/components/WizardContinueButton.tsx
+++ web/src/wizard/components/WizardContinueButton.tsx
@@ -10,7 +10,10 @@ interface Props {
 
 const WizardContinueButton: React.FC<Props> = ({ link, onClick, disabled }) => {
     return (
-        <Link to={link} className="container xl:max-w-screen-lg fixed bottom-0">
+        <Link
+            to={link}
+            className={`container xl:max-w-screen-lg fixed bottom-0 ${disabled ? "pointer-events-none" : ""}`}
+        >
             <Button onClick={onClick} fullWidth disabled={disabled}>
                 Continue
             </Button>
