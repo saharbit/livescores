11bf64677141235eb1e5605ebaca9c430c8ba7d8
Sahar <sahar.bit@gmail.com>
2020-11-20 15:15:40 +0200

    Remove unused deps

--- web/src/modules/wizard/components/WizardListItem.tsx
+++ web/src/modules/wizard/components/WizardListItem.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import SelectItemButton from "../../../common/SelectItemButton";
 
 type Props = {
     name: string;
